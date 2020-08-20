5d9ffa23e6ff76be507b3eea94fd215488d94475
m10an <vn.mlshk@gmail.com>
2020-08-20 10:29:50 +0300

    Multiple typedef styles (for Go and C)

--- gotypes.h
+++ gotypes.h
@@ -2,13 +2,16 @@
 
 // int
 // TODO: choose between int32 and int64 depending on system (32-bit or a 64-bit machine)
+typedef int64_t intgo_t;
 typedef int64_t GoInt;
 
-// float  <=> float32
-// double <=> float64
+// float
+typedef float float32_t;
 typedef float GoFloat32;
 
-// slice
-typedef const struct { void      *ptr; GoInt len; GoInt cap; } GoSlice;
-typedef const struct { GoFloat32 *ptr; GoInt len; GoInt cap; } GoFloat32Slice;
-typedef const struct { GoInt     *ptr; GoInt len; GoInt cap; } GoIntSlice;
+// float32 slice
+typedef const struct _GoFloat32Slice { float32_t *ptr; intgo_t len; intgo_t cap; } float32_slice_t;
+typedef float32_slice_t GoFloat32Slice;
+// int slice
+typedef const struct _GoIntSlice { intgo_t   *ptr; intgo_t len; intgo_t cap; } intgo_slice_t;
+typedef intgo_slice_t GoIntSlice;
